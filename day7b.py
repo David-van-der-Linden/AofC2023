915631d13f0f93b0e2f7a36f69171be4b841162d
David-van-der-Linden <61505634+David-van-der-Linden@users.noreply.github.com>
2023-12-07 22:47:14 +0100

    spelling fixes

--- day7b.py
+++ day7b.py
@@ -1,12 +1,13 @@
 import math
 import re
 from my_secrets import path
+from collections import Counter
 
 input_file = 'input7.txt'
 
 # compare hands
 # print('A'>='')
-# if we map capetal leters to lowercase letters we can use string comparison for which hand wins given there the same type
+# if we map capital letters to lowercase letters we can use string comparison for which hand wins given there the same type
 cap_to_lower = {'A': 'z',
 				'K': 'y',
 				'Q': 'x',
@@ -14,15 +15,15 @@ cap_to_lower = {'A': 'z',
 				'T': 'v'}
 # print('1234z'>='1234y')
 
-# idea parse all replace letters acording to cap_to_lower
+# idea parse all replace letters according to cap_to_lower
 # add a symbol at the start indicating which hand type it is and then apply a sorting algorithm to it
 
-def determin_type(str):
-	# input str is a string of lenght 5 
-	# returns single carater string between a and g 
-	# where g corrsponds to the best hand type and a to the worst hand type
+def determine_type(str):
+	# input str is a string of length 5 
+	# returns single character string between a and g 
+	# where g corresponds to the best hand type and a to the worst hand type
 	
-	# dictonary that counts how often a character occurs in a string
+	# dictionary that counts how often a character occurs in a string
 	temp_dict = {}
 	for car in str:
 		# print(car)
@@ -33,45 +34,43 @@ def determin_type(str):
 	
 	# print(temp_dict)
 
-    # akey will be the caracter that occurs the most and bkey the caracter that occurs the second most
-    # maxa and maxb will be temp_dict[akey] and temp_dict[bkey] respectivly
-	maxa = 0
-	maxb = 0
-	akey = 'none'
-	bkey = 'none'
+    # a_key will be the character that occurs the most and b_key the character that occurs the second most,
+    # a_val and b_val will be temp_dict[a_key] and temp_dict[b_key] respectively.
+	a_val = 0
+	b_val = 0
+	a_key = 'none'
+	b_key = 'none'
 	jokers = 0
 	
 	for key in temp_dict:
 		if key == '.':
 			jokers = temp_dict[key]
-		elif temp_dict[key] >= maxa:
-			maxb = maxa
-			bkey = akey
-			maxa = temp_dict[key]
-			akey = key
-		elif temp_dict[key] > maxb:
-			maxb = temp_dict[key]
-			bkey = key
+		elif temp_dict[key] >= a_val:
+			b_val = a_val
+			b_key = a_key
+			a_val = temp_dict[key]
+			a_key = key
+		elif temp_dict[key] > b_val:
+			b_val = temp_dict[key]
+			b_key = key
 
-	# print(akey)
-
-	# logic for determing which hand we have
-	if maxa + jokers == 5:
+	# logic for determining which hand type we have
+	if a_val + jokers == 5:
 		#type 5 of a kind
 		return 'g'
-	elif maxa + jokers == 4:
+	elif a_val + jokers == 4:
 		# type 4 of a kind
 		return 'f'
-	elif (maxa + jokers == 3 and maxb == 2) or (maxa == 3 and maxb + jokers == 2):
+	elif (a_val + jokers == 3 and b_val == 2) or (a_val == 3 and b_val + jokers == 2):
 		# type full house
 		return 'e'
-	elif maxa + jokers == 3:
+	elif a_val + jokers == 3:
 		# type 3 of a kind
 		return 'd'
-	elif maxa == 2 and maxb == 2:
+	elif a_val == 2 and b_val == 2:
 		# type 2 pair
 		return 'c'
-	elif maxa + jokers == 2:
+	elif a_val + jokers == 2:
 		# type 1 pair
 		return 'b'
 	else:
@@ -87,17 +86,17 @@ with open(path + input_file) as file:
 for key in cap_to_lower:
 	filedata = filedata.replace(key, cap_to_lower[key])
 # Write the file out again
-with open(path + 'prosessed_' + input_file, 'w') as file:
+with open(path + 'processed_' + input_file, 'w') as file:
   file.write(filedata)
 
 # ------------------ adding the symbol at the start of the line ---------
 import fileinput
 import sys
 
-for line in fileinput.input([path + 'prosessed_' + input_file], inplace=True):
-    sys.stdout.write('{t}{l}'.format(l=line, t=determin_type(line.split()[0])))
+for line in fileinput.input([path + 'processed_' + input_file], inplace=True):
+    sys.stdout.write('{t}{l}'.format(l=line, t=determine_type(line.split()[0])))
 
-with open(path + 'prosessed_' + input_file) as f:
+with open(path + 'processed_' + input_file) as f:
     lines = f.read().split('\n')
 
 lines.sort()
