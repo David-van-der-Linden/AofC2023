e35f8b03d1f5c19b9f359983700e8a502efdf083
David-van-der-Linden <61505634+David-van-der-Linden@users.noreply.github.com>
2023-12-13 15:06:29 +0100

    Cleaned day 5

--- day5a.py
+++ day5a.py
@@ -1,7 +1,5 @@
-import re
 import numpy as np
 from my_secrets import path
-import time
 
 with open(path + 'input5.txt') as f:
     blocks = f.read().split('\n\n')
@@ -11,27 +9,18 @@ seeds = [int(seed) for seed in blocks[0].split(': ')[1].split()]
 for block_index in range(1,len(blocks)):
     blocks[block_index] = blocks[block_index].split('\n')
 
-min_key = 99999999999999999999999999999
+# idea follow reasoning just like in the examples for all possible seeds
 
-tik = time.time()
+min_key = np.inf
 for seed in seeds:
     key = seed
-    print('seed', key)
     for block_index in range(1,len(blocks)):
         block = blocks[block_index]
-        # print('block_index', block_index)
         for line_index in range(1,len(block)):
-            # print('line index:', line_index)
-            destination_range_start, source_range_start, range_lenght = int(block[line_index].split()[0]), int(block[line_index].split()[1]), int(block[line_index].split()[2])
-            if key >= source_range_start and key < source_range_start + range_lenght:
+            destination_range_start, source_range_start, range_length = int(block[line_index].split()[0]), int(block[line_index].split()[1]), int(block[line_index].split()[2])
+            if key >= source_range_start and key < source_range_start + range_length:
                 key = destination_range_start + key - source_range_start
-                # print('key', key)
-                # print('time to break!')
                 break
     min_key = min(min_key,key)
     
 print(min_key)
-
-tak = time.time()
-print(tak-tik)
-print(len(seeds))
--- day5b.py
+++ day5b.py
@@ -1,4 +1,5 @@
 from my_secrets import path
+import numpy as np
 
 with open(path + 'input5.txt') as f:
     blocks = f.read().split('\n\n')
@@ -13,77 +14,62 @@ for block_index in range(1,len(blocks)):
 # then seed 9 has a slack of 4
 # and seed 10 has an slack of 0
 # neutral_slack comes from identity transformations
-# neutral_slack = key - max(source_r_s + range_lenght - 1 st its les than key) + 1
+# neutral_slack = key - max(source_r_s + range_length - 1 st its les than key) + 1
 # active_slack comes form using an interval to transform to the next key and 
-# then the slack is how much lower it could have been while stil using the same interval
+# then the slack is how much lower it could have been while still using the same interval
 # so when 
 # 10, 5, 5
 # with key 6 the slack is 1
 # active_slack = key - source_r_s
-# per transformation there will be slack ither the neutral or ative slack
-# the total slack will be the miniumum of all of them
+# per transformation there will be slack either the neutral or active slack
+# the total_slack (or slack for short) will be the minimum of all of them
 # 
-# foor each seed there is a total_slack, and we know that
+# for each seed there is a total_slack, and we know that
 # loc(seed-total_slack) = loc(seed) - total_slack
 #
 # because of this we only need to compute first at the top of the interval
-# next_seed = seed - totalslack - 1
+# next_seed = seed - total_slack - 1
 # given next seed is still in the interval if not then
-# the bottem of the interval can be computed and compared to the seed - total_slacks
+# the bottom of the interval can be computed and compared to the seed - total_slacks
 
-locmin = 999999999999999999999999999999
+loc_min = np.inf
 
 nr_of_seeds = 0
 for range_index in range(int(len(ranges)/2)):
     nr_of_seeds += ranges[range_index*2+1]
 
-print(nr_of_seeds)
-
 for range_index in range(int(len(ranges)/2)):
     range_min = ranges[range_index*2]
-    print('range_min', range_min)
     range_max = ranges[range_index*2]+ranges[range_index*2+1]-1
-    print('range_max', range_max)
 
-    # init
     seed = range_max
-    while seed >= range_min:
-        def get_loc():
-            key = seed
-            slack = 999999999999999999999999999999999999999999
-            # print('seed', key)
-            for block_index in range(1,len(blocks)):
-                block = blocks[block_index]
-                # print('block_index', block_index)
-                neutral_slack = 99999999999999999999999999999
-                for line_index in range(1,len(block)):
-                    # print('line index:', line_index)
-                    destination_range_start, source_range_start, range_lenght = int(block[line_index].split()[0]), int(block[line_index].split()[1]), int(block[line_index].split()[2])
-                    if key >= source_range_start and key < source_range_start + range_lenght:
-                        # active slack
-                        slack = min(slack, key-source_range_start)
-                        key = destination_range_start + key - source_range_start
-                        # print('key', key)
-                        # print('time to break!')
-                        break
-                    elif source_range_start + range_lenght <= key:
-                        neutral_slack = min(neutral_slack, key - source_range_start - range_lenght + 1)
-                # neutral slack
-                slack = min(slack, neutral_slack)
-            return key, slack
+    while True:
+        key = seed
+        total_slack = np.inf
+        for block_index in range(1,len(blocks)):
+            block = blocks[block_index]
+            neutral_slack = np.inf
+            for line_index in range(1,len(block)):
+                destination_range_start, source_range_start, range_length = int(block[line_index].split()[0]), int(block[line_index].split()[1]), int(block[line_index].split()[2])
+                if key >= source_range_start and key < source_range_start + range_length:
+                    # since in interval we update active slack
+                    active_slack = key-source_range_start
+                    total_slack = min(total_slack, active_slack)
+                    key = destination_range_start + key - source_range_start
+                    break
+                elif source_range_start + range_length <= key:
+                    # if not in interval update neutral slack
+                    neutral_slack = min(neutral_slack, key - source_range_start - range_length + 1)
+            total_slack = min(total_slack, neutral_slack)
         
-        loc, slack = get_loc()
+        loc, slack = key, total_slack
         if seed == range_min:
-            # print('if')
-            locmin = min(locmin,loc)
+            loc_min = min(loc_min, loc)
             break
         elif seed - slack > range_min:
-            # print('elif')
-            locmin = min(locmin,loc - slack)
+            loc_min = min(loc_min, loc - slack)
             seed = seed - slack - 1
         else:
-            # print('else')
             seed = range_min
-    
-    
-print('locmin', locmin)
+
+print('loc_min', loc_min)
