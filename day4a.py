9e91e314539af96b7e573bfc06fe3ea7f4e394ed
David-van-der-Linden <61505634+David-van-der-Linden@users.noreply.github.com>
2023-12-13 14:03:55 +0100

    Cleaned day 4

--- day4a.py
+++ day4a.py
@@ -6,25 +6,16 @@ with open(path + 'input4.txt') as f:
 
 ans = 0
 for line in lines:
-    # print(line)
     split_line = line.split(':')[1]
     split_line = split_line.split('|')
-    #  print(split_line)
-
-    winning_numbers = re.findall('[0-9]+', split_line[0])
-    numbers_on_card = re.findall('[0-9]+', split_line[1])
-    # print('winning_numbers', winning_numbers)
-    # print('numbers_on_card', numbers_on_card)
+    winning_nrs = re.findall('[0-9]+', split_line[0])
+    nrs_on_card = re.findall('[0-9]+', split_line[1])
 
     nr_of_matches = 0
-    for nr in numbers_on_card:
-        if nr in winning_numbers:
-            # print(nr)
+    for nr in nrs_on_card:
+        if nr in winning_nrs:
             nr_of_matches += 1
     
     if nr_of_matches > 0:
         ans = ans + 2 ** (nr_of_matches-1)
-    # print('nrofmachtes', nr_of_matches)
-    # print(2 ^ (nr_of_matches-1))
-    # print('ansthus far', ans)
 print(ans)
