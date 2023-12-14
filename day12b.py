0db099bcebc9beba6d112ebe7d0ab53246e039d9
David-van-der-Linden <61505634+David-van-der-Linden@users.noreply.github.com>
2023-12-14 09:24:04 +0100

    Added ball check
    ball check
    len(ssl) vs len(seq_to_min_ssl(seq))

--- day12b.py
+++ day12b.py
@@ -15,14 +15,14 @@ with open(path + 'input12.txt') as f:
 # lets say we have a sequence
 # seq = [1,2,3]
 # then the shortest string matching it would be
-# sorest_str[seq] = '#.##.###'
-# len(sorest_str[seq]) = sum(seq) + len(seq) - 1
+# seq_to_min_ssl(seq) = '#.##.###'
+# len(seq_to_min_ssl(seq)) = sum(seq) + len(seq) - 1
 #                      = 6 + 3 - 1 = 8
 # if our string of spring labels is
 # ssl = '????????',     len(ssl) = 8
 # then there is only one way for this seq ssl pair to match
 # ssl = '?????????',    len(ssl) = 9
-# then balls = len(ssl) - len(sorest_str[seq]) = 1
+# then balls = len(ssl) - len(seq_to_min_ssl(seq)) = 1
 # this is how much leeway we have this leeway can be inserted
 # into the shortest sting in one of the following ways
 # '.#.##.###', '#..##.###', '#.##..###', '#.##.###.'
@@ -33,6 +33,9 @@ with open(path + 'input12.txt') as f:
 # that all strings of spring labels (ssls) have no . at the start 
 # since nr_of_ways(ssl,seq) = nr_of_ways('.'+ssl, seq) for all ssl and seq
 # and no consecutive .s since they could be replaced by a single .
+
+# whenever we have a ssl that is leading with an # we can truncate since we know that
+# the first # is part of the first element of seq
 
 def is_feasible(str_with_q, str_without_q):
     # example
@@ -48,9 +51,20 @@ def is_feasible(str_with_q, str_without_q):
             return False
     return True
 
+def seq_to_min_ssl(seq : list):
+    ssl = ''
+    for i in seq:
+        ssl = ssl + '#'*i + '.'
+    return ssl[:-1]
+
 def nr_of_ways(ssl : str, seq : list):
     if ssl == '':
         return 1 if seq == [] else 0
+    # if len(seq_to_min_ssl(seq)) > len(ssl):
+    elif sum(seq) + len(seq) - 1 > len(ssl):
+        return 0
+    elif sum(seq) + len(seq) - 1 == len(ssl):
+        return 1 if is_feasible(ssl,seq_to_min_ssl(seq)) else 0
     elif ssl[0] == '.':
         return nr_of_ways(ssl[1:], seq)
     elif ssl[0] == '?':
@@ -75,27 +89,30 @@ def nr_of_ways(ssl : str, seq : list):
     else:
         assert False 
 
-# ans = 0
-# for line in lines:
-#     split_line = line.split()
-#     temp_str = split_line[0]
-#     ssl = f'{temp_str}?{temp_str}?{temp_str}?{temp_str}?{temp_str}'
-#     temp_seq = [int(val) for val in split_line[1].split(',')]
-#     seq = temp_seq + temp_seq + temp_seq + temp_seq + temp_seq
+ans = 0
+for line in lines:
+    split_line = line.split()
+    temp_str = split_line[0]
+    ssl = f'{temp_str}?{temp_str}?{temp_str}?{temp_str}?{temp_str}'
+    temp_seq = [int(val) for val in split_line[1].split(',')]
+    seq = temp_seq + temp_seq + temp_seq + temp_seq + temp_seq
     
-#     print('line', line)
-#     print('ssl', ssl)
-#     print('seq', seq)
+    print('line', line)
+    print('ssl', ssl)
+    print('seq', seq)
     
-#     ways = nr_of_ways(ssl,seq)
-#     ans += ways
+    ways = nr_of_ways(ssl,seq)
+    ans += ways
 
-#     print('ways', ways)
+    print('ways', ways)
 
-# print('ans:', ans)
+print('ans:', ans)
 
 # 322650 is to low
-print(nr_of_ways('???????#??????##.?'*2+'???????#??????##.', [1,3,2,1,2,1,3,2,1,2,1,3,2,1,2]))
+# print(nr_of_ways('???????#??????##.?'*2+'???????#??????##.', [1,3,2,1,2,1,3,2,1,2,1,3,2,1,2]))
+# print(nr_of_ways('???????#??????##.????????#??????##.????????#??????##.????????#??????##.????????#??????##.', 
+#                  [1, 3, 2, 1, 2, 1, 3, 2, 1, 2, 1, 3, 2, 1, 2, 1, 3, 2, 1, 2, 1, 3, 2, 1, 2]))
+
 # line ???????#??????##. 1,3,2,1,2
 # ssl = '???????#??????##.????????#??????##.????????#??????##.????????#??????##.????????#??????##.'
 # seq = [1, 3, 2, 1, 2, 1, 3, 2, 1, 2, 1, 3, 2, 1, 2, 1, 3, 2, 1, 2, 1, 3, 2, 1, 2]
