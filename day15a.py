0708bf61ff9ebfbc9fc2d520e91c04968d666714
David-van-der-Linden <61505634+David-van-der-Linden@users.noreply.github.com>
2023-12-15 10:03:02 +0100

    Day 15a done

---
+++ day15a.py
@@ -0,0 +1,18 @@
+from my_secrets import path
+
+with open(path + 'input15.txt') as f:
+    steps = f.read().split(',')
+
+def get_hash(step):
+    current_value = 0
+    for char in step:
+        current_value += ord(char)
+        current_value = current_value*17
+        current_value = current_value % 256
+    return current_value
+
+ans = 0
+for step in steps:
+    ans += get_hash(step)
+
+print(ans)
