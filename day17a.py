fa7ad59f57977f74731c79e175841bd6824f5b67
David-van-der-Linden <61505634+David-van-der-Linden@users.noreply.github.com>
2023-12-17 18:58:30 +0100

    Made graph directional

--- day17a.py
+++ day17a.py
@@ -73,7 +73,7 @@ class Graph:
             self.add_vertex(to)
 
         self.vert_dict[frm].add_neighbor(self.vert_dict[to], cost)
-        self.vert_dict[to].add_neighbor(self.vert_dict[frm], cost)
+        # self.vert_dict[to].add_neighbor(self.vert_dict[frm], cost) # we want a directed graph
 
     def get_vertices(self):
         return self.vert_dict.keys()
